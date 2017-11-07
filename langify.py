c20faff163c486a0598c6fe7e4aebfc697e56a99
jordim <info@useit.es>
2017-11-07 17:10:22 +0100

    removed output folder

--- langify.py
+++ langify.py
@@ -1,8 +1,9 @@
+import os
+
 from managers.core import CoreManager
 
 import config
 
+os.system("export GOOGLE_APPLICATION_CREDENTIALS='service_account.json'")
 core_manager = CoreManager(config.params)
 core_manager.translate()
-
-#export GOOGLE_APPLICATION_CREDENTIALS="service_account.json"
